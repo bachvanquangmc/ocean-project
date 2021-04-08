c57516e1d57ce26d2188e7bb64f7b75cce081352
ChenWarren <hchen273@my.bcit.ca>
2021-04-08 11:26:39 -0700

    add bgcolor to pages index

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ import Category from '../comps/CategoryUI'
 
 const Home = styled.div`
     width: 100vw;
+    heigh: 100vh;
     display: flex;
     flex-direction: column;
+    background-color: #333;
 `
 
 export default function HomeBody() {
