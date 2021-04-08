6d7136389fc985aca5af9e89103fd7f8b99f1836
ChenWarren <hchen273@my.bcit.ca>
2021-04-07 20:11:38 -0700

    add hidden menu comp

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 // import your comps here
+import HiddenMenu from '../comps/HiddenMenu'
+import MenuBar from '../comps/MenuBar'
 
 const Home = styled.div`
     width: 100vw;
@@ -10,6 +12,11 @@ const Home = styled.div`
 export default function HomeBody() {
   return<Home>
           {/* comps go here */}
-
+            <HiddenMenu></HiddenMenu>
+            <MenuBar></MenuBar>
+            <MenuBar barText='Importance'></MenuBar>
+            <MenuBar barText='Threats'></MenuBar>
+            <MenuBar barText='Protection'></MenuBar>
+            <HiddenMenu></HiddenMenu>
     </Home>
 }
