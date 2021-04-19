0db1239457290ba3d58ef7a5f248dfe5c005cdd4
BlenHaile <bhaile300@gmail.com>
2021-04-18 23:36:01 -0700

    created paragraph component for intro pgs

---
+++ comps/IntroTextUI/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ParaInput = styled.p`
+    color:#D3D3D3;   
+    text-align: center;
+    margin:10px;
+`;
+
+const Para = () => {
+
+    return <div>
+        <ParaInput>Body Text</ParaInput>
+    </div>
+}
+
+export default Para;
