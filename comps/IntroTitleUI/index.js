b8e93e95a2d21279c2073108ce33002552760642
BlenHaile <bhaile300@gmail.com>
2021-04-18 23:35:36 -0700

    created h1 compnent for intro pgs

---
+++ comps/IntroTitleUI/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const H1Input = styled.h1`
+    color:#ffffff;   
+    text-align: center;
+    text-shadow: 0px 2px 2px black;
+    margin:10px;
+`;
+
+const H1 = () => {
+
+    return <div>
+        <H1Input>Section Title</H1Input>
+    </div>
+}
+
+export default H1;
