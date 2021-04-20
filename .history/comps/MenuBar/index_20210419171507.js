9dcd4ba5c9cc7a9432bb6d8337d768c22e5d9ed8
ChenWarren <hchen273@my.bcit.ca>
2021-04-19 17:40:17 -0700

    router in s3page, adjustment on index.js

---
+++ .history/comps/MenuBar/index_20210419171507.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import {useRouter} from 'next/router'
+
+const MenuBarWraper = styled.div`
+    height: 80px;
+    width: 100vw;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 0px 0 5px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+`
+
+const MenuText = styled.h1`
+    font-size: 24px;
+    text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2);
+`
+
+const MenuBar = ({
+    barText = 'Home'
+    m
+}) => {
+
+    const menuRouter = useRouter()
+
+    return <MenuBarWraper>
+            <MenuText>{barText}</MenuText>
+        </MenuBarWraper>
+} 
+
+export default MenuBar
