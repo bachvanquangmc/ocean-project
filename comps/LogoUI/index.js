5241345f3c71dd60f2d484dc8a2f2b0d42847988
MaharlikaC <mclomata@my.bcit.ca>
2021-04-22 19:21:07 -0700

    adjustments

---
+++ comps/LogoUI/index.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import AppName from '../AppNameUI'
+
+
+const LogoWraper = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 50px;
+`
+const LogoImg = styled.div`
+    width: 70%;
+    transform: rotate(${props => props.rotate}deg);
+    transition: transform 2s;
+`
+const LogoUI = (
+    rotate = '0'
+) =>
+{
+    const [rotateState, setRotate] = useState(false)
+
+    useEffect(()=>{
+        setRotate(true)
+    })
+
+    return<LogoWraper>
+        <LogoImg rotate={ rotateState ? 360 : 0 } >
+            <img src='logo_white.svg' />
+        </LogoImg>
+        <AppName text="Edu-Ocean"></AppName>
+    </LogoWraper>
+}
+
+export default LogoUI
