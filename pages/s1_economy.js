db83a2a278546bba66320f3ba1a3dbcb3804a3aa
MaharlikaC <mclomata@my.bcit.ca>
2021-05-13 13:37:10 -0700

    updated economy page

--- pages/s1_economy.js
+++ pages/s1_economy.js
@@ -11,15 +11,16 @@ const Body = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
-    background: rgb(4,191,173);
-    background: linear-gradient(180deg, rgba(4,191,173,1) 0%, rgba(25,149,201,1) 60%, rgba(11,78,138,1) 100%);
+    background: rgb(170,215,255);
+    background: url(/ocean_background_2.svg) no-repeat center center fixed;
+    background-size: cover;
 `
 
 export default function S1_economy() {
     const router = useRouter();
   return<Body>
             <Header></Header>
-            <PageLayout FTitle="Growing Economy" STitle="How does the ocean contribute?" FDiscription="Over 3 billion people depend on marine and coastal biodiversity to sustain their livelihoods." SDiscription="There are many things relating to the ocean that affects our economy. The most obvious natural recource is the fishery, in that it provides us with seafood. The ocean also contributes with energy generation, raw minerals through subsea mining, pearl production, medicinal material from marine animals and plants, transportation of supplies, travel routes and recreation/tourism, and much more." BackgroundImg="" ImgSource="/shipyard.svg"></PageLayout>
+            <PageLayout FTitle="Growing Economy" STitle="How does the ocean contribute?" FDiscription="Over 3 billion people depend on marine and coastal biodiversity to sustain their livelihoods." SDiscription="There are many things relating to the ocean that affects our economy. The most obvious natural recource is the fishery, in that it provides us with seafood. The ocean also contributes with energy generation, raw minerals through subsea mining, pearl production, medicinal material from marine animals and plants, transportation of supplies, travel routes and recreation/tourism, and much more." BackgroundImg="" ImgSource="/economy_graph.svg"></PageLayout>
             <div onClick={()=>router.push('/part2_intro')}>
                 <NextButton></NextButton>
             </div>
